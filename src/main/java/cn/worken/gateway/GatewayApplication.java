81016981ed4fbd4d4f69d184dddbf9c46ec10d19
邵益炯 <shaoyijiong@worken.cn>
2020-07-03 15:06:52 +0800

    Initial commit

---
+++ src/main/java/cn/worken/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package cn.worken.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author shaoyijiong
+ * @date 2020/7/3
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
